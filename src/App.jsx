b666650123fad9d0bfd19bebffb0c31e2d38eaed
vishnu <vishnubhel@rediffmail.com>
2023-01-10 11:28:45 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React,{useState}  from 'react';
 
 const App=()=>{
     const[name, setName]= useState("");
+    const[name1, setName1]= useState("");
 const[fullName, setfullName]=useState();
+const[fullName1, setfullName1]=useState();
 
 
 
@@ -13,16 +15,38 @@ const[fullName, setfullName]=useState();
         setName(event.target.value);
     }
 
-    const onSubmit=()=>{
-        setfullName(name);
+    const inputEvent1=(event1)=>{  // yahn ye event isliye likha h jb button click hoga toh vo ek objt release kre ga toh usko catch krne k liye 
+        console.log(event1.target.value);
+        setName1(event1.target.value);
     }
+
+
+
+
+
+    const onSubmit=(event)=>{
+        event.preventDefault();
+       // event1.preventDefault();
+        setfullName(name);
+        setfullName1(name1);
+    }
+
+    // const onSubmit=()=>{
+    //     setfullName1(name1);
+    // }
 
    return(
         <>
         <div>
-        <h1>Hello {fullName}</h1>
+        <form onSubmit={onSubmit}>
+        <h1>Hello {fullName} {fullName1}</h1>
         <input type="text" placeholder='Enter your name' onChange={inputEvent} value={name}></input>
-        <button onClick={onSubmit}>Submit</button>
+
+        <input type="text" placeholder='Enter your name' onChange={inputEvent1} value={name1}></input>
+
+        <button type="submit">Submit</button>
+       
+        </form>
         </div>
         </>
     );
